c7fee3332470f726232090dd5f05103e87352691
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-04-20 16:44:47 +0100

    Add home page

--- src/modules/getlikes.js
+++ src/modules/getlikes.js
@@ -1,13 +1,13 @@
-// const getLikes = async () => {
-//   const getApiLike = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8QDj3NEWg5KCn5q9XXe5/likes';
-//   const response = await fetch(getApiLike);
-//   const allLikes = await response.json();
-//   allLikes.forEach((el) => {
-//     if (document.querySelector(`[data-id="${el.item_id}"]`)) {
-//       document.querySelector(
-//         `[data-id="${el.item_id}"]`,
-//       ).nextElementSibling.innerHTML = el.likes;
-//     }
-//   });
-// };
-// export default getLikes;
+const getLikes = async () => {
+  const getApiLike = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8QDj3NEWg5KCn5q9XXe5/likes';
+  const response = await fetch(getApiLike);
+  const allLikes = await response.json();
+  allLikes.forEach((el) => {
+    if (document.querySelector(`[data-id="${el.item_id}"]`)) {
+      document.querySelector(
+        `[data-id="${el.item_id}"]`,
+      ).nextElementSibling.innerHTML = el.likes;
+    }
+  });
+};
+export default getLikes;
